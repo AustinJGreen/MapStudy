740965207d4f8a37728b30e28d2598ba0748916a
ajosg <austin.green@outlook.com>
2018-03-21 00:46:12 -0700

    Merge with refractored project

--- MapGenerator/Properties/AssemblyInfo.cs
+++ MapGenerator/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MapStudyResources")]
+[assembly: AssemblyTitle("MapGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MapStudyResources")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("MapGenerator")]
+[assembly: AssemblyCopyright("Copyright © Austin Green 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
